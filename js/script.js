f990d905e2c08bd9b635442930f6c62fb8c03924
valerie_l <contacttovalerie@gmail.com>
2023-08-23 01:51:46 -0400

    text can change color

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-var pizzaType = "veggie";
+//var pizzaType = "veggie";
 
-var slicesEaten = 4;
+//var slicesEaten = 4;
 
-console.log(`The ${pizzaType} pizza has ${8 - slicesEaten} slices left.`);
+//console.log(`The ${pizzaType} pizza has ${8 - slicesEaten} slices left.`);
 
 //The veggie pizza has 4 slices left.
+
+  /* Header styles */
+
+
+const heading = document.getElementById("mainHeader");
+
+// Array of colors
+const colors = ["#FF5733", "#FFC300", "#85C1E9", "#58D68D", "#F39C12", "#D98880", "#BB8FCE"];
+  
+heading.addEventListener("click", function () {
+
+// Random color generation
+
+const randomIndex = Math.floor(Math.random() * colors.length);
+    
+// Change text color
+heading.style.color = colors[randomIndex];
+});
